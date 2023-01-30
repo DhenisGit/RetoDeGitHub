dee8d5b2c752b06485743f9ebb5d0808f4ce02ad
Dhenis <dhenis@mail.com>
2023-01-30 15:54:46 -0500

    Agregando cambios a mi repositorio

--- app/src/main/java/dhenis/example/subir_github/MainActivity.java
+++ app/src/main/java/dhenis/example/subir_github/MainActivity.java
@@ -3,12 +3,22 @@ package dhenis.example.subir_github;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    public void Mensaje(View view){
+        Toast.makeText(this, "Buenas Tardes Bienvenido", Toast.LENGTH_SHORT).show();
     }
 }
